02028b97a5d019d2c8a5d97183ae15cbc07a09d0
Alexandre Tomy <ale.tomy1996@gmail.com>
2020-07-24 17:31:18 -0300

    1a pagina alvo: extraindo infos (s/ tratamento) das maquinas da vultr

---
+++ src/crawler_clouds/vultr.py
@@ -0,0 +1,30 @@
+from base import traz_conteudo, pega_arvore
+from lxml import etree
+
+URL = "https://www.vultr.com/products/cloud-compute/"
+
+XPATH_LINHAS = "//div[@id='compute']//div[@data-row]"
+XPATH_COLUNA = "./div[$col]//text()"
+
+def crawl():
+
+    c = traz_conteudo(URL)
+
+    arvore = pega_arvore(c)
+
+    txts_col = etree.XPath(XPATH_COLUNA)
+
+    for div in arvore.xpath(XPATH_LINHAS):
+
+        yield {
+            "cpu": txts_col(div, col = 3)[2],
+            "mem": txts_col(div, col = 4)[1],
+            "sto": txts_col(div, col = 2)[2],
+            "band": txts_col(div, col = 5)[2],
+            "price": txts_col(div, col = 6)[2]
+        }
+
+
+if __name__ == "__main__":
+    for item in crawl():
+        print(item)
