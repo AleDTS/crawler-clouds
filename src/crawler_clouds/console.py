65fd02309b2890f647dc084c74c3d87391190002
Alexandre Tomy <ale.tomy1996@gmail.com>
2020-07-24 18:21:44 -0300

    imprime itens na tela com comando --print

--- src/crawler_clouds/console.py
+++ src/crawler_clouds/console.py
@@ -1,11 +1,27 @@
 import click
 
 from . import __version__
+from . import vultr
+
+crawlers = {
+    "vultr": vultr,
+}
+
+
+def imprime(nome_crawler):
+    crawler = crawlers[nome_crawler]
+    for item in crawler.crawl():
+        click.echo(item)
 
 
 @click.command()
 @click.version_option(version=__version__)
-def main(crawler):
+@click.argument('crawler')
+@click.option('--print', is_flag=True, help="Imprime resultados na tela")
+def main(crawler, print):
     """Crawler de informações para máquinas cloud"""
-    click.echo("Olar!")
-    
+    if crawler not in ['vultr']:
+        raise click.BadParameter("Argumento inválido")
+
+    if print:
+        imprime(crawler)
--- src/crawler_clouds/vultr.py
+++ src/crawler_clouds/vultr.py
@@ -1,10 +1,11 @@
-from base import traz_conteudo, pega_arvore
+from .base import traz_conteudo, pega_arvore
 from lxml import etree
 
 URL = "https://www.vultr.com/products/cloud-compute/"
 
 XPATH_LINHAS = "//div[@id='compute']//div[@data-row]"
 XPATH_COLUNA = "./div[$col]//text()"
+
 
 def crawl():
 
@@ -17,14 +18,9 @@ def crawl():
     for div in arvore.xpath(XPATH_LINHAS):
 
         yield {
-            "cpu": txts_col(div, col = 3)[2],
-            "mem": txts_col(div, col = 4)[1],
-            "sto": txts_col(div, col = 2)[2],
-            "band": txts_col(div, col = 5)[2],
-            "price": txts_col(div, col = 6)[2]
+            "cpu": txts_col(div, col=3)[2],
+            "mem": txts_col(div, col=4)[1],
+            "sto": txts_col(div, col=2)[2],
+            "band": txts_col(div, col=5)[2],
+            "price": txts_col(div, col=6)[2]
         }
-
-
-if __name__ == "__main__":
-    for item in crawl():
-        print(item)
