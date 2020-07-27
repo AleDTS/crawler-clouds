770e5fb0e4ef28b8caab8b20c5b69dae072a0995
Alexandre Tomy <ale.tomy1996@gmail.com>
2020-07-27 09:43:42 -0300

    salva saida em .json

--- src/crawler_clouds/console.py
+++ src/crawler_clouds/console.py
@@ -1,4 +1,6 @@
 import click
+import json
+import os
 
 from . import __version__
 from . import vultr
@@ -14,14 +16,34 @@ def imprime(nome_crawler):
         click.echo(item)
 
 
+def salva_json(nome_crawler):
+    crawler = crawlers[nome_crawler]
+
+    arquivo = f'{nome_crawler}.json'
+    itens = []
+
+    for item in crawler.crawl():
+        itens.append(item)
+
+    if os.path.exists(arquivo):
+        os.remove(arquivo)
+
+    with open(arquivo, 'w') as f:
+        f.write(json.dumps(itens))
+
+
 @click.command()
 @click.version_option(version=__version__)
 @click.argument('crawler')
 @click.option('--print', is_flag=True, help="Imprime resultados na tela")
-def main(crawler, print):
+@click.option('--save_json', is_flag=True, help="Salva dados em arquivo json")
+def main(crawler, print, save_json):
     """Crawler de informações para máquinas cloud"""
     if crawler not in ['vultr']:
         raise click.BadParameter("Argumento inválido")
 
     if print:
         imprime(crawler)
+
+    if save_json:
+        salva_json(crawler)
